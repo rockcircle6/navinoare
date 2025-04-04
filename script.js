4fb75c756af63ce1b39593984594adf4865eb1da
rockcircle6 <rockcircle6@gmail.com>
2025-04-05 02:32:54 +0900

    js機能追加

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Highway app started!");
+navigator.geolocation.getCurrentPosition(position => {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  document.body.innerHTML += `<p>現在地: 緯度 ${lat}, 経度 ${lon}</p>`;
+});
+const highwayData = {
+  "東名高速道路": {
+    start: { lat: 35.171, lon: 136.881 },
+    facilities: [{ type: "SA", name: "海老名SA", kp: 45 }]
+  }
+};
